45d08c182d798203dfa7085b6f30152a52ff830b
Suhku Huh <nineclue@gmail.com>
2024-12-11 06:47:44 +0900

    some error handling; still has problem

--- src/main.js
+++ src/main.js
@@ -1,31 +1,49 @@
 const { invoke } = window.__TAURI__.core;
-const { Window } = window.__TAURI__.window;
+const { Window, CloseRequestEvent } = window.__TAURI__.window;
 const { Webview } = window.__TAURI__.webview;
 
 let urlInputEl;
 let urlMsgEl;
 
 async function greet() {
-  let openUrl = 'https://'.concat(urlInputEl.value)
-  urlMsgEl.textContent = "Let's Open ".concat(openUrl)
+  const urlHead = 'https://'
+  let openUrl;
+  if (urlInputEl.value.length > 0) {
+    if (urlInputEl.value.slice(0, 7) === urlHead) {
+      openUrl = urlInputEl;
+    } else {
+      openUrl = urlHead.concat(urlInputEl.value);
+    }
+    urlMsgEl.textContent = "Let's Open ".concat(openUrl);
 
-  const appWindow = new Window('uniqueLabel');
+    const appWindow = new Window('urlWin');
+    const appSize = await appWindow.innerSize();
+  
+    const webview = new Webview(appWindow, 'urlWinWV', {
+      url: openUrl,
+      parent: "main",
+      x: 0, 
+      y: 0,
+      width: appSize.width,
+      height: appSize.height
+    });
 
-  const webview = new Webview(appWindow, 'theUniqueLabel', {
-    url: openUrl,
-    parent: "main",
-    x: 50, 
-    y: 50,
-    width: 200,
-    height: 200
-  });
+    appWindow.listen('tauri://close-requested', function (e) {
+      console.log('close requested', e)
+      e.preventDefault();
+      appWindow.hide();
+    });
 
-  webview.once('tauri://created', function () {
-    console.log("Successful loading")
-  });
-  webview.once('tauri://error', function (e) {
-    console.log("failed to launch ", e.payload)
-   });
+    webview.once('tauri://created', function () {
+      console.log("Successful loading")
+      appWindow.show()
+    });
+    webview.once('tauri://error', function (e) {
+      console.log("failed to launch ", e.payload)
+     });  
+  } else {
+    urlMsgEl.textContent = 'Please enter a valid URL'
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
