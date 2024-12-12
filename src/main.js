12132ed0d4e764b5ee31bb82ced5363dc4fee415
nineclue <nineclue@gmail.com>
2024-12-12 14:40:13 +0900

    works with some limitations; added icons

--- src/main.js
+++ src/main.js
@@ -4,42 +4,65 @@ const { Webview } = window.__TAURI__.webview;
 
 let urlInputEl;
 let urlMsgEl;
+const wvMap = new Map();
 
 async function greet() {
   const urlHead = 'https://'
+  const winLabel = 'urlWin'
+  const wvLabel = 'urlWv'
+
   let openUrl;
-  if (urlInputEl.value.length > 0) {
-    if (urlInputEl.value.slice(0, 7) === urlHead) {
+  const url = urlInputEl.value
+  const urlKey = url.replace(".", "_")
+  if (url.length > 0) {
+    if (url.slice(0, 7) === urlHead) {
       openUrl = urlInputEl;
     } else {
-      openUrl = urlHead.concat(urlInputEl.value);
+      openUrl = urlHead.concat(url);
     }
     urlMsgEl.textContent = "Let's Open ".concat(openUrl);
 
-    const appWindow = new Window('urlWin');
-    const appSize = await appWindow.innerSize();
-  
-    const webview = new Webview(appWindow, 'urlWinWV', {
-      url: openUrl,
-      parent: "main",
-      x: 0, 
-      y: 0,
-      width: appSize.width,
-      height: appSize.height
-    });
+    let appWindow = await Window.getByLabel(winLabel);
+    if (appWindow === null) {
+      console.log('window is not created yet')
+      appWindow = new Window(winLabel)
+    } else {
+      console.log('window is already created', appWindow)
+      appWindow.show()
+    };
 
-    appWindow.listen('tauri://close-requested', function (e) {
+    // const appWindow = new Window('urlWin');
+    const appSize = await appWindow.innerSize();
+
+    let webview;
+    if (wvMap.has(urlKey)) {
+      console.log('webview already created')
+      webview = wvMap.get(urlKey)
+      webview.reparent(appWindow)
+    } else {
+      console.log('webview is not created yet')
+      webview = new Webview(appWindow, urlKey, {
+        url: openUrl,
+        x: 0, 
+        y: 0,
+        width: appSize.width,
+        height: appSize.height
+      })
+      wvMap.set(urlKey, webview)
+    }
+ 
+    appWindow.onCloseRequested(function (e) {
       console.log('close requested', e)
       e.preventDefault();
       appWindow.hide();
     });
-
     webview.once('tauri://created', function () {
       console.log("Successful loading")
       appWindow.show()
     });
     webview.once('tauri://error', function (e) {
       console.log("failed to launch ", e.payload)
+      appWindow.show()
      });  
   } else {
     urlMsgEl.textContent = 'Please enter a valid URL'
